91ea33efa1173bc41cb80fd5a5cdc1ac47399f8e
Sethupathi <mysethu888@gmail.com>
2024-01-02 14:42:31 +0530

    adss

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,19 +6,20 @@ import { Helmet } from 'react-helmet'
 
 const About = () => {
     return (
-        <div>
-            <Helmet>
-                <meta property="og:site_name" content="Sethu Portfolio About page" />
-                <meta property="og:title" content="Sethuyadav" />
-                <meta property="og:description" content="About sethu" />
-                <meta property="og:image" content="https://thumbs.dreamstime.com/b/web-development-coding-programming-internet-technology-business-concept-web-development-coding-programming-internet-technology-121903546.jpg" />
-                <meta property="og:type" content="website" />
-                <meta property="og:image:type" content="image/jpg" />
-                <meta property="og:image:width" content="300" />
-                <meta property="og:image:height" content="300" />
-                <meta property="og:url" content="https://main--bucolic-crisp-4bfe2d.netlify.app/#about" />
-            </Helmet>
+      
             <section className={style.container} id='about'>
+                <Helmet>
+                    {/* <meta name="description" content="About-Page" /> */}
+                    <meta property="og:site_name" content="Sethu Portfolio About page" />
+                    <meta property="og:title" content="Sethuyadav" />
+                    <meta property="og:description" content="About sethu" />
+                    <meta property="og:image" content="https://thumbs.dreamstime.com/b/web-development-coding-programming-internet-technology-business-concept-web-development-coding-programming-internet-technology-121903546.jpg" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:image:type" content="image/jpg" />
+                    <meta property="og:image:width" content="300" />
+                    <meta property="og:image:height" content="300" />
+                    <meta property="og:url" content="https://main--bucolic-crisp-4bfe2d.netlify.app/#about" />
+                </Helmet>
                 <h2 className={style.title}>About</h2>
                 <div className={style.content}>
                     <img src={getImageUrl("about/aboutImage.png")} alt=" sitting with lap" className={style.aboutImage} />
@@ -53,7 +54,6 @@ const About = () => {
                     </ul>
                 </div>
             </section>
-        </div>
     )
 }
 

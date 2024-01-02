c6676b5d5e2914b941e2765708e11be020639473
Sethupathi <mysethu888@gmail.com>
2024-01-02 17:10:35 +0530

    news

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
           <title>Home</title>
           <meta property="og:title" content="SethuPathi M - Home" />
           <meta property="og:description" content="SEthu" />
-          <meta property="og:image" content="https://thumbs.dreamstime.com/b/web-development-coding-programming-internet-technology-business-concept-web-development-coding-programming-internet-technology-121903546.jpg" />
+          {/* <meta property="og:image" content="https://thumbs.dreamstime.com/b/web-development-coding-programming-internet-technology-business-concept-web-development-coding-programming-internet-technology-121903546.jpg" /> */}
           <meta property="og:type" content="website" />
           <meta property="og:image:type" content="image/jpg" />
           <meta property="og:image:width" content="300" />
